c185fe5d29944228f8a364a42f44c0170022ea5a
stoodx <siewsoft@gmail.com>
2015-12-18 11:54:10 +0200

    Start coding

--- TrainsWatchMobile/app/src/main/java/stoodx/trainswatchmobile/MainActivity.java
+++ TrainsWatchMobile/app/src/main/java/stoodx/trainswatchmobile/MainActivity.java
@@ -2,6 +2,8 @@ package stoodx.trainswatchmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner)findViewById(R.id.spinnerFromA);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinnerFromA, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
     }
 }
