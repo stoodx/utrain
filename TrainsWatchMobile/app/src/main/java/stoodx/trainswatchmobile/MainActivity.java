62784cf0bfdcda32ebeec0fffb9b7ce1b2f56b17
stoodx <siewsoft@gmail.com>
2015-12-22 11:22:01 +0200

    Created fillStations() method

--- TrainsWatchMobile/app/src/main/java/stoodx/trainswatchmobile/MainActivity.java
+++ TrainsWatchMobile/app/src/main/java/stoodx/trainswatchmobile/MainActivity.java
@@ -1,11 +1,29 @@
 package stoodx.trainswatchmobile;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    public class Station {
+        public String m_strID;
+        public String m_strName;
+    }
+
+    private Station[] m_arrayStationsFrom;
+    private Station[] m_arrayStationsTo;
+
+    private Spinner m_spinnerFromA;
+    private Spinner m_spinnerToA;
+    private Spinner m_spinnerFrom;
+    private Spinner m_spinnerTo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,17 +31,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //From
-        Spinner spinnerFromA = (Spinner)findViewById(R.id.spinnerFromA);
+        m_spinnerFromA = (Spinner)findViewById(R.id.spinnerFromA);
+        m_spinnerFromA.setOnItemSelectedListener(this);
         ArrayAdapter<CharSequence> adapterFromA = ArrayAdapter.createFromResource(this,
                 R.array.spinnerFromA, android.R.layout.simple_spinner_item);
         adapterFromA.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spinnerFromA.setAdapter(adapterFromA);
+        m_spinnerFromA.setAdapter(adapterFromA);
+
+        m_spinnerFrom = (Spinner)findViewById(R.id.spinnerFrom);
+        ArrayAdapter<CharSequence> adapterFrom = ArrayAdapter.createFromResource(this,
+                R.array.spinnerFrom, android.R.layout.simple_spinner_item);
+        adapterFrom.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        m_spinnerFrom.setAdapter(adapterFrom);
+
 
         //To
-        Spinner spinnerToA = (Spinner) findViewById(R.id.spinnerToA);
+        m_spinnerToA = (Spinner) findViewById(R.id.spinnerToA);
+        m_spinnerToA.setOnItemSelectedListener(this);
         ArrayAdapter<CharSequence> adapterToA = ArrayAdapter.createFromResource(this,
                 R.array.spinnerToA, android.R.layout.simple_spinner_item);
         adapterToA.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spinnerToA.setAdapter(adapterToA);
+        m_spinnerToA.setAdapter(adapterToA);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+
+        int iID = parent.getId();
+        switch (iID){
+            case R.id.spinnerFromA:
+                fillStations(m_spinnerFromA, m_spinnerFrom, m_arrayStationsFrom);
+                break;
+            case R.id.spinnerToA:
+                messageBox("Увага", "ToA " + m_spinnerToA.getItemAtPosition(pos));
+                break;
+            default:
+                return;
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+    private void messageBox(String strTitle, String strMessage){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setTitle(strTitle)
+                .setMessage(strMessage)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton("Ок", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private boolean fillStations(Spinner spinnerA, Spinner spinner,  Station[] arr ){
+        //clear all lists
+        arr = new Station[0];
+        ArrayAdapter adap = (ArrayAdapter) spinner.getAdapter();
+        adap.clear();
+        adap.notifyDataSetChanged();
+        return true;
     }
 }
